8dd0b61adbebbaca2d3919e101da20bc8398da25
jjeleven11 <40149119+jjeleven11@users.noreply.github.com>
2018-07-14 15:48:45 +0200

    Add files via upload

---
+++ exo1.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+
+
+def scrap
+    var = {}
+    page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    links = page.css("a").select{|link| link['class'] == "lientxt"}
+    links.each do |link|
+        url = "http://annuaire-des-mairies.com#{link['href']}"
+        link.each do |email|
+            doc = Nokogiri::HTML(open(url))
+            email = doc.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+            var.merge!(link: url, mail: email)
+        end
+        puts var
+    end
+end
+
+scrap
